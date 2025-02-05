fd41db144bb885ec85d7c41d260a713edf91c1bd
brainrot <fufhwkjdjjfmdmmf@gmail.com>
2025-02-05 10:38:39 +0200

    new allowed host

--- writeit/writeit/settings.py
+++ writeit/writeit/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "default-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'writeit.ua']
 
 
 # Application definition
